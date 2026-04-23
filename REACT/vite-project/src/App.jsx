98efbc8c9e6cf4ce9b51d80f6e4d5b4576aee1a1
ANSHUMAN YADAV <iamanshumanyadav@gmail.com>
2026-04-23 12:21:12 +0530

    Twenty Third Commit

---
+++ REACT/vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>This is my first React app</p>
+      <Cse12/>
+    </div>
+  );
+}
+// 2nd Component
+function Cse12(){
+  return (
+    <div className="Cse12">
+      <h2>Welcome to CSE 12</h2>
+      <p>This is some content for2nd Component</p>
+    </div>
+  );
+}
+export default App;
