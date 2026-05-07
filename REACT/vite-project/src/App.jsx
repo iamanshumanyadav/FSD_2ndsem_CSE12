14668c2f94e003543601920220ad6d1686047a8b
ANSHUMAN YADAV <iamanshumanyadav@gmail.com>
2026-05-07 10:52:45 +0530

    Twenty Fourth Commit

--- REACT/vite-project/src/App.jsx
+++ REACT/vite-project/src/App.jsx
@@ -1,18 +1,30 @@
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World</h1>
+//       <p>This is my first React app</p>
+//       <Cse12/>
+//     </div>
+//   );
+// }
+// // 2nd Component
+// function Cse12(){
+//   return (
+//     <div className="Cse12">
+//       <h2>Welcome to CSE 12</h2>
+//       <p>This is some content for2nd Component</p>
+//     </div>
+//   );
+// }
+// export default App;
+
+import Student from "./Student";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <p>This is my first React app</p>
-      <Cse12/>
-    </div>
-  );
-}
-// 2nd Component
-function Cse12(){
-  return (
-    <div className="Cse12">
-      <h2>Welcome to CSE 12</h2>
-      <p>This is some content for2nd Component</p>
+      <h1>Student Information</h1>
+      <Student name="Alice" course="CSE12" marks={85} />
     </div>
   );
 }
